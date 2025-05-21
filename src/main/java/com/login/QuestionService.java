495e440af0f28be981eea255f1a3cdfe454dea95
IT24103980 <IT24103980@my.sliit.lk>
2025-05-22 02:25:30 +0530

    Add files via upload

---
+++ src/main/java/com/login/QuestionService.java
@@ -0,0 +1,17 @@
+package com.exammanagement.controller;
+
+import com.exammanagement.model.Question;
+
+public class QuestionService {
+    public Question addQuestion(Question question) {
+        return question;
+    }
+
+    public Question getQuestionById(String questionId) {
+        return null;
+    }
+
+    public boolean updateQuestion(Question existingQuestion) {
+        return false;
+    }
+}
